1650e932c767f6dbe1086b5ccd74e82ea9ee638f
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-10-09 09:06:24 -0700

    changed var randomIndex

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//var Rules= confirm("Guess the Current Word below by typing any letter. You have 15 tries for each word.");
+//var Rules= confirm("Guess the Current Word below by typing any letter. You have 10 tries for each word.");
 
    var wordList=[
   "whoville",
@@ -13,7 +13,8 @@
 ];
 
 //make random wordbank chooser--> chooses word from list randomly
-var randomIndex = Math.floor(Math.random() * wordList.length) - 1;
+var randomIndex = Math.floor(Math.random() * wordList.length);
+console.log(randomIndex);
 
 // randomWordToGuess is the random word chosen from the array
 var randomWordToGuess= wordList[randomIndex];
@@ -21,27 +22,26 @@ console.log(randomWordToGuess);
 
 // need to generate underscores for the length of randomWordToGuess
  var wordDashes= [];
-// for (let randomIndex = 0; i < randomIndex.length; randomIndex++) {
-        wordDashes[randomWordToGuess]="_";   
+
+    wordDashes[randomWordToGuess]="_";   
  for (let randomIndex = 0; randomIndex < randomWordToGuess.length; randomIndex++) {
-    const element = randomWordToGuess[randomIndex];
+     const element=randomWordToGuess[randomIndex];
 console.log(wordDashes);
 };
 
-    
-// }
-// console.log(wordDashes);
+// create array to store used words and another to store used letters
+//display dashes in current word
+// create function to capture keystrokes
+
 
 
 // https://stackoverflow.com/questions/47787313/javascript-creating-a-guess-the-word-game
 
-// or is it better to make an array for each word?
 
-var wordOne=['w', 'h','o', 'v', 'i', 'l', 'l', 'e'];
 
-let guessesLeft=15;
+let guessesLeft=10;
 let wins=0;
-//let lettersGuessed= ?
+let lettersGuessed= []
 
 const htmlNode= document.getElementById('');
 //should i make a sep div? not sure what to put here
