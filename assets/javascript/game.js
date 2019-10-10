96a905a47c0a10e2b99b4ba4462ae2c93669e2e7
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-10-10 08:22:23 -0700

    got dashes to display, made separate file of things I tried but aren't working now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,41 @@
-//var Rules= confirm("Guess the Current Word below by typing any letter. You have 10 tries for each word.");
+    //var Rules= confirm("Guess the Current Word below by typing any letter. You have 10 tries for each word.");
 
-   var wordList=[
-  "whoville",
-  "christmas",
-  "green",
-  "max",
-  "mt crumpit",
-  "cindy lou",
-  "heart",
-  "jazzercise",
-  "hate"
-];
+    var wordList=[
+    "whoville",
+    "christmas",
+    "green",
+    "max",
+    "mt crumpit",
+    "cindy lou",
+    "heart",
+    "jazzercise",
+    "hate"
+    ];
 
-//make random wordbank chooser--> chooses word from list randomly
-var randomIndex = Math.floor(Math.random() * wordList.length);
-console.log(randomIndex);
+    //make random wordbank chooser--> chooses word from list randomly
+    var randomIndex = Math.floor(Math.random() * wordList.length);
+    console.log(randomIndex);
 
-// randomWordToGuess is the random word chosen from the array
-var randomWordToGuess= wordList[randomIndex];
-console.log(randomWordToGuess);
+    // randomWordToGuess is the random word chosen from the array
+    var randomWordToGuess= wordList[randomIndex];
+    console.log(randomWordToGuess);
 
-// need to generate underscores for the length of randomWordToGuess
- var wordDashes= [];
+    const htmlNode= document.getElementById("container");
+    // need to generate underscores for the length of randomWordToGuess
+    var wordDashes=[];
 
-    wordDashes[randomWordToGuess]="_";   
- for (let randomIndex = 0; randomIndex < randomWordToGuess.length; randomIndex++) {
-     const element=randomWordToGuess[randomIndex];
-console.log(wordDashes);
+        wordDashes[randomWordToGuess]="_";   
+    for (let randomIndex = 0; randomIndex < randomWordToGuess.length; randomIndex++) {
+            const element= randomWordToGuess[randomIndex];
+        wordDashes.push("_");
+        wordDashes.toString()
 
-
-
-});
-
-//display dashes in current word
-    // document.getElementById("")
-
-var dashBox=document.getElementById("dash-box");
-
-wordDashes.forEach(function(dashes){
+    document.getElementById("dash-box").innerHTML=wordDashes;
+    console.log(wordDashes);
+    };
     
-    var iDontKnow=document.createElement("display");
 
-    iDontKnow.textContent=dashes;
+    document.onkeyup=function(event){
+    var userGuess= event.key;
+    }
 
-
-    dashBox.appendChild(iDontKnow);
-    
-console.log(dashBox);
-};
-
-
-// wordDashes.textContent=" ";
-
-// var emptySpace=document.createElement("display")
-// emptySpace.textContent=" ";
-
-// wordDashes.appendChild(emptySpace);
-
-// wordDashes.for(listItem => {
-//     const emptySpace=document.createElement("display");
-//     emptySpace.textContent=listItem;
-//     wordDashes.appendChild(emptySpace);
-// });
-
-// create array to store used words and another to store used letters
-    //lettersGuessed=store letters
-    //can use .push to modify?
-
-
-// create function to capture keystrokes
-
-
-
-// https://stackoverflow.com/questions/47787313/javascript-creating-a-guess-the-word-game
-
-
-
-let guessesLeft=10;
-let wins=0;
-let lettersGuessed= []
-
-const htmlNode= document.getElementById('');
-//should i make a sep div? not sure what to put here
-
-document.onkeyup=function(event){
-  var userGuess= event.key;
-}
---
+++ assets/javascript/tried.js
@@ -0,0 +1,47 @@
+// stuff that isn't working now but might later?
+
+
+
+// var wins=0;
+    // document.getElementById("wins").innerHTML=wins;
+
+    // var guessesLeft=10;
+    // document.getElementById("guesses-left").innerHTML=guessesLeft;
+
+    // var lettersGuessed= [];
+    // document.getElementById("letters-guessed")
+
+
+    //
+    // const htmlNode= document.getElementById('dash-box');
+    // htmlNode.innerHTML=(wordDashes);
+
+    
+
+
+    // var emptySpace=document.createElement("display")
+            // emptySpace.textContent=" ";
+
+            // wordDashes.appendChild(emptySpace);
+
+            // wordDashes.for(listItem => {
+            //     const emptySpace=document.createElement("display");
+            //     emptySpace.textContent=listItem;
+            //     wordDashes.appendChild(emptySpace);
+            // });
+
+    // create array to store used words and another to store used letters
+        //lettersGuessed=store letters
+        //can use .push to modify?
+
+
+    // create function to capture keystrokes
+
+
+
+    // https://stackoverflow.com/questions/47787313/javascript-creating-a-guess-the-word-game
+
+
+
+
+    
